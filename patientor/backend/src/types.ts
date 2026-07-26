42c4d198aca02e2e5f45bff3d4974894dfb01941
dan <self.destructed.dan@gmail.com>
2026-07-26 19:07:49 +0300

    feat(patientor): add proper Entry types with discriminated union (ex25step3)

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -24,9 +24,52 @@ export const newPatientSchema = z.object({
 
 export type NewPatient = z.infer<typeof newPatientSchema>;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+const HealthCheckRating = {
+  Healthy: 0,
+  LowRisk: 1,
+  HighRisk: 2,
+  CriticalRisk: 3,
+} as const;
+type HealthCheckRating = typeof HealthCheckRating[keyof typeof HealthCheckRating];
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
 
 export interface Patient extends NewPatient {
   id: string;
